c0feebcdf474dc9b5848ce5f86f078a85688beed
encode-peter <peter@ziotag.com>
2025-12-16 16:54:15 -0500

    Updated Hero section and theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ export default {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        "luminary-gold": "hsl(var(--luminary-gold))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
